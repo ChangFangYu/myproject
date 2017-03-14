30b12216c51ad61cf8c067ff3a0c8b6c154e799f
user <user@es302n14>
2017-03-14 15:55:55 +0800

    新增建構子(Constructor)

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -5,4 +5,13 @@ public class Car {
       String name;
       int cc;
       String type;
+      int status;
+      
+      public Car (String brand, String name, int cc){
+    	  this.brand= brand;
+    	  this.name = name;
+    	  this.cc = cc;
+      }
 }
+
+

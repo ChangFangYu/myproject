2709d9499af92898383bbff668ad80bb599b2e8e
Annie <Annie@Annie>
2017-03-17 15:13:39 +0800

    This is practice (2)

--- src/oo/abstraction/Clothes.java
+++ src/oo/abstraction/Clothes.java
@@ -4,4 +4,10 @@ public class Clothes {
 	String color;
 	String size;
 	String type;
+	
+	public Clothes ( String color, String size, String type){
+		this.color = color;
+		this.size = size;
+		this.type = type;
+	}
 }
--- src/oo/abstraction/ClothesTester.java
+++ src/oo/abstraction/ClothesTester.java
@@ -3,8 +3,8 @@ package oo.abstraction;
 public class ClothesTester {
 
 	public static void main(String[] args) {
-		Clothes c1 = new  Clothes();
-
+		Clothes a1 = new Clothes ("blue", "S", "plaid blouse");
+		Clothes a2 = new Clothes ("white", "S", "striped blouse");
+		Clothes a3 = new Clothes ("red", "S", "chiffon blouse");	
 	}
-
 }

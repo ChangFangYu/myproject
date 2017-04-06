60ba5c29a0ae77bd014639cb4eef6c12aee9097a
ChangFangYu <annie.chang.10002@gmail.com>
2017-04-06 15:36:48 +0800

    practice3

--- src/oo/test/Invoice.java
+++ src/oo/test/Invoice.java
@@ -1,6 +1,7 @@
 package oo.test;
 
 import java.util.ArrayList;
+import java.lang.String;
 import java.util.Scanner;
 
 public class Invoice {
@@ -11,9 +12,29 @@ public class Invoice {
           a.add("821");
           a.add("886");
           a.add("554");
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("請輸入發票號碼:");
-          String num = scanner.nextLine();
+          String my_str = " ";
+          boolean flag = false;
+          do{
+        	  Scanner scanner = new Scanner(System.in);
+              System.out.println("請輸入發票號碼:");
+              my_str = scanner.nextLine();
+              if (my_str.length()!=8) {
+                      System.out.println("發票號碼有誤，請重新輸入:");
+              }        
+          }
+          while (my_str.length()!=8);
+          String get_str = my_str.substring(5, 8);
+          for (int i=0;i<4;i++){
+              flag=get_str.equals(a.get(i));
+              System.out.print(a.get(i));
+         
+            if (flag){
+              System.out.print(" --中獎囉! ");
+            }
+              else {
+              System.out.print(" --沒中獎! ");
+              }
+   
+           }
 	}
-
 }

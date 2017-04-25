7a0062464ec7514ec64f03b7b59803b18db1fedb
user <user@es302n14>
2017-04-25 14:42:48 +0800

    refresh

--- src/oo/mid/Meal.java
+++ src/oo/mid/Meal.java
@@ -3,14 +3,25 @@ package oo.mid;
 import java.util.Scanner;
 
 public class Meal {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int price;
+        int price;
 		String numbers;
-		System.out.println("請輸入點餐號碼:");
+		String snack;
+		public Meal(int price, String numbers, String snack) {
+			super();
+			this.price = price;
+			this.numbers = numbers;
+			this.snack = snack;
+		}
+		public void print(){
+			System.out.println("請輸入點餐號碼:");
+		}
+	    public int discount(){
+	    	int dis = (int)(price*0.9f);
+	    	return dis;
+	    }
+		
 		
 
-	}
-
 }
+
+
--- src/oo/mid/Ticket.java
+++ src/oo/mid/Ticket.java
@@ -3,15 +3,27 @@ package oo.mid;
 import java.util.ArrayList;
 
 public class Ticket {
-	public static void main(String[] args) {
-		ArrayList<String> a = new ArrayList<>();
-		int id;
+        int id;
 		String name;
 		float price;
-		a.add("kids");
-		a.add("teenagers");
-		a.add("adults");
-		a.add("elders");
+		public void setId(int id){
+			this.id = id;
+		}
+		public int getId(){
+			return id;
+		}
+		public void setName(String name){
+			this.name = name;
+		}
+		public String getName(){
+			return name;
+		}
+		public void setPrice(float price){
+			this.price = price;
+		}
+		public float getPrice(){
+			return price;
+		}
 		
-	}
+	
 }

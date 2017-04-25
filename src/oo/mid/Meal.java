01afb89a84c8b250c0e3ffad1e2f68f60e97b86f
user <user@es302n14>
2017-04-25 14:25:06 +0800

    midterm test

---
+++ src/oo/mid/Meal.java
@@ -0,0 +1,16 @@
+package oo.mid;
+
+import java.util.Scanner;
+
+public class Meal {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int price;
+		String numbers;
+		System.out.println("請輸入點餐號碼:");
+		
+
+	}
+
+}
---
+++ src/oo/mid/Ticket.java
@@ -0,0 +1,17 @@
+package oo.mid;
+
+import java.util.ArrayList;
+
+public class Ticket {
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<>();
+		int id;
+		String name;
+		float price;
+		a.add("kids");
+		a.add("teenagers");
+		a.add("adults");
+		a.add("elders");
+		
+	}
+}
